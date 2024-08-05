5b95667ac0ce6ee5115ba43beffca043c63d2f22
Yosuke Hanaoka <35240048+yoshan0921@users.noreply.github.com>
2024-08-04 20:24:56 -0700

    Changed tracking mode from hands to holistic

--- main.py
+++ main.py
@@ -2,35 +2,50 @@ import cv2
 import mediapipe as mp
 
 # Set up MediaPipe
-mp_hands = mp.solutions.hands
-hands = mp_hands.Hands()
+mp_holistic = mp.solutions.holistic
 mp_drawing = mp.solutions.drawing_utils
 
 # Create object for capturing video using the OpenCV
 cap = cv2.VideoCapture(0)
 
-while cap.isOpened():
-    ret, frame = cap.read()
-    if not ret:
-        break
+with mp_holistic.Holistic(
+    min_detection_confidence=0.5, min_tracking_confidence=0.5
+) as holistic:
+    while cap.isOpened():
+        ret, frame = cap.read()
+        if not ret:
+            break
 
-    # OpenCV handles images in BGR format by default, so convert to RGB format
-    frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        # OpenCV handles images in BGR format by default, so convert to RGB format
+        frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
 
-    # Execute hand tracking
-    results = hands.process(frame_rgb)
+        # Execute hand tracking
+        results = holistic.process(frame_rgb)
 
-    # Draw hand landmarks
-    if results.multi_hand_landmarks:
-        for hand_landmarks in results.multi_hand_landmarks:
-            mp_drawing.draw_landmarks(frame, hand_landmarks, mp_hands.HAND_CONNECTIONS)
+        # Draw detected landmarks
+        if results.pose_landmarks:
+            mp_drawing.draw_landmarks(
+                frame, results.pose_landmarks, mp_holistic.POSE_CONNECTIONS
+            )
+        if results.face_landmarks:
+            mp_drawing.draw_landmarks(
+                frame, results.face_landmarks, mp_holistic.FACEMESH_TESSELATION
+            )
+        if results.left_hand_landmarks:
+            mp_drawing.draw_landmarks(
+                frame, results.left_hand_landmarks, mp_holistic.HAND_CONNECTIONS
+            )
+        if results.right_hand_landmarks:
+            mp_drawing.draw_landmarks(
+                frame, results.right_hand_landmarks, mp_holistic.HAND_CONNECTIONS
+            )
 
-    # Show result
-    cv2.imshow("Hand Tracking", frame)
+        # Show result
+        cv2.imshow("Hand Tracking", frame)
 
-    # Break the loop when 'q' is pressed
-    if cv2.waitKey(10) & 0xFF == ord("q"):
-        break
+        # Break the loop when 'q' is pressed
+        if cv2.waitKey(10) & 0xFF == ord("q"):
+            break
 
 cap.release()
 cv2.destroyAllWindows()
